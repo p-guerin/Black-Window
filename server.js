69961d647aab6a144eb34e7a6b5215789f6bd574
p-guerin <guerin.pierre26@gmail.com>
2015-09-01 11:27:21 +0200

    Configuration du module body-parser

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-var express = require('express');
-var path    = require('path');
+var express      = require('express');
+var path         = require('path');
+var bodyParser   = require('body-parser');
 
-var app     = express();
+var app          = express();
 
 
 // Settings
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
 // Routing
 app.get('/', function(req,res){
