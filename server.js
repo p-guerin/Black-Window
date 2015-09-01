36883f81ee6e23b86bf5d55c61aadc190f2522f0
p-guerin <guerin.pierre26@gmail.com>
2015-09-01 12:59:38 +0200

    Ajout des routes contact et nouveau-projet && Ajout des tests && Ajout variable port

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var path         = require('path');
 var bodyParser   = require('body-parser');
 
 var app          = express();
+var port         = process.env.PORT || 3000;
 
 
 // Settings
@@ -12,11 +13,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+
 // Routing
 app.get('/', function(req,res){
-  res.render('index')
+  console.log('Index');
+  res.render('index');
+});
+
+app.get('/contact', function(req,res){
+  console.log('Contact');
+  res.render('contact');
+});
+
+app.get('/nouveau-projet', function(req,res){
+  console.log('Nouveau-projet');
+  res.render('nouveau-projet');
 });
 
 
 // Server
-app.listen(3000);
+app.listen(port);
