88b48492cd088ba15ff445eb2de455a1fdee6b11
p-guerin <guerin.pierre26@gmail.com>
2015-09-01 14:09:05 +0200

    Configuration du module nodemailer && création des routes post pour les pages contact et nouveau-projet

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express      = require('express');
 var path         = require('path');
 var bodyParser   = require('body-parser');
+var nodemailer   = require('nodemailer');
 
 var app          = express();
+var transporter  = nodemailer.createTransport();
 var port         = process.env.PORT || 3000;
 
 
@@ -24,10 +26,30 @@ app.get('/contact', function(req,res){
   console.log('Contact');
   res.render('contact');
 });
+app.post('/contact', function(req,res){
+  transporter.sendMail({
+    from: req.body.email,
+    to: 'guerin.pierre26@gmail.com',
+    subject: 'Contact depuis le site Pierre GUERIN.',
+    text: req.body.message
+  });
+  console.log('Send contact');
+  res.redirect('/');
+});
 
 app.get('/nouveau-projet', function(req,res){
   console.log('Nouveau-projet');
   res.render('nouveau-projet');
+});
+app.post('/nouveau-projet', function(req,res){
+  transporter.sendMail({
+    from: req.body.email,
+    to: 'guerin.pierre26@gmail.com, ' + req.body.email,
+    subject: 'Devis projet.',
+    text: req.body.message
+  });
+  console.log('Send contact');
+  res.redirect('/');
 });
 
 
